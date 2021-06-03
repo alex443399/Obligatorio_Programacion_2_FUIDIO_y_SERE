a662bfabd04778ad40eefcb2a16f41c32a2602fc
Federico Fuidio <ffuidio@correo.um.edu.uy>
2021-06-03 17:49:32 -0300

    variable definitions in CastMember

--- src/Modelo/CastMember.java
+++ src/Modelo/CastMember.java
@@ -1,4 +1,25 @@
 package Modelo;
 
+import java.time.LocalDate;
+
 public class CastMember {
+
+    private String imbdNameId;
+    private String name;
+    private String birthName;
+    private int height;
+    private String bio;
+    private LocalDate birthDate;
+    private String birthCountry;
+    private String birthCity;
+    private LocalDate deathDate;
+    private String deathState;
+    private String deathCountry;
+    private String deathCity;
+    private String spousesString;
+    private int spouses;
+    private int divorces;
+    private int spousesWithChildren;
+    private int children;
+
 }
