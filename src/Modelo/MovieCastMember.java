4fb4049c165b368bc473415924ca22fe39eb2cb3
Federico Fuidio <ffuidio@correo.um.edu.uy>
2021-06-04 19:09:09 -0300

    Defincion de entidades en el modelo

--- src/Modelo/MovieCastMember.java
+++ src/Modelo/MovieCastMember.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class MovieCastMember {
 
+    private String imbdNameId;
+    private String imbdTitled;
     private int ordering;
     private String category;
     private String job;
--- src/Modelo/MovieRating.java
+++ src/Modelo/MovieRating.java
@@ -4,10 +4,28 @@ import java.util.List;
 
 public class MovieRating {
 
+    private String imbdTitled;
     private float weightedAverage;
     private int totalVotes;
     private float meanVotes;
     private float medianVote;
     private List<Integer> votesRating;
+    private Rating allGenders_0age;
+    private Rating allGenders_18age;
+    private Rating allGenders_30age;
+    private Rating allGenders_45age;
+    private Rating malesAllAge;
+    private Rating males_0age;
+    private Rating males_18age;
+    private Rating males_30age;
+    private Rating males_45age;
+    private Rating femalesAllAge;
+    private Rating females_0age;
+    private Rating females_18age;
+    private Rating females_30age;
+    private Rating females_45age;
+    private Rating top_1000Voters;
+    private Rating usVoters;
+    private Rating nonUsVoters;
 
 }
