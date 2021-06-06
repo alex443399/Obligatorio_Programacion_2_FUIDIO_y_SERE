c17982335827744ff135df36896a44a4522d84cb
Alejandro Sere <alex443399@gmail.com>
2021-06-06 15:50:35 -0300

    Defino el ultimo loader

--- src/Utilidades/Loader.java
+++ src/Utilidades/Loader.java
@@ -222,5 +222,63 @@ public class Loader {
         return rating_storage;
     }
 
+    public OpenHash<Integer, Movie> load_movie_cast_member(int debbug_text) throws Exception{
+
+        int number_of_columns = 22, number_of_rows = 85854;
+
+        int hash_size = (int) (number_of_rows/hash_load_factor);
+
+        OpenHash<Integer, Movie> movie_storage = new OpenHash(hash_size);
+
+        if(debbug_text>0) System.out.println("Loading Movies Begining...");
+
+        String path = "C:\\Users\\alex4\\IdeaProjects\\Obligatorio Programacion 2 v1\\src\\Files\\IMDb movies.csv";
+
+        String delimiter = ",";
+
+        BufferedReader br = new BufferedReader(new FileReader(path));
+
+        String header_line = br.readLine();
+
+        String[] headers = header_line.split(",");
+
+        String line = "";
+        int i = 0;
+        int error_counter = 0;
+        int inserted_counter = 0;
+
+        while((line = br.readLine()) != null){// && i < 100
+            i++;
+
+            String[] registro = Functions.StringArrayFromCsvLine(line,',','"',number_of_columns);
+
+            int name_string_length = registro[0].length();
+            String imbd_title_id_string = registro[0].substring(2,name_string_length);
+            int imdb_title_id = Integer.parseInt(imbd_title_id_string);
+
+
+            if(debbug_text>2) System.out.println("Begining to load: tt" + imbd_title_id_string + ", fraction: " + Integer.toString(i) + "/" + Integer.toString(number_of_rows));
+            try {
+                Movie movie_to_load = new Movie(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5], registro[6], registro[7], registro[8], registro[9], registro[10], registro[11], registro[12], registro[13], registro[14], registro[15], registro[16], registro[17], registro[18], registro[19], registro[20], registro[21]);
+
+                movie_storage.put(
+                        imdb_title_id,
+                        movie_to_load
+                );
+                inserted_counter++;
+            }
+            catch (Exception e){
+                if(debbug_text>2) System.out.println("Error cargando durante la iteracion: " + Integer.toString(i) + " con titulo imdb: " + registro[0]);
+                error_counter++;
+            }
+
+            if(debbug_text>2) System.out.println("Loaded tt " + Integer.toString(imdb_title_id) + ", fraction: " + Integer.toString(i) + "/" + Integer.toString(number_of_rows));
+        }
+        if(debbug_text>0) System.out.println("Loading Movies Ended");
+        //if(debbug_text>0) print time
+        if(debbug_text>1) System.out.println("Errores: " + error_counter);
+        if(debbug_text>1) System.out.println("Inserciones: " + inserted_counter);
+        return movie_storage;
+    }
 
 }
