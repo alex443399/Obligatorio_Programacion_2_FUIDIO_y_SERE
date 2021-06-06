f373c81fead6ecfa928b6b65687d54ca2ff3541a
Federico Fuidio <ffuidio@correo.um.edu.uy>
2021-06-06 15:22:46 -0300

    Constructores y getters que faltaban

--- src/Modelo/MovieCastMember.java
+++ src/Modelo/MovieCastMember.java
@@ -9,6 +9,39 @@ public class MovieCastMember {
     private int ordering;
     private String category;
     private String job;
-    private List<String> characters;
+    private String[] characters;
 
+    public MovieCastMember(String imbdNameId, String imbdTitled, String ordering,
+                           String category, String job, String characters){
+        this.imbdNameId = imbdNameId;
+        this.imbdTitled = imbdTitled;
+        this.ordering = Integer.parseInt(ordering);
+        this.category = category;
+        this.job = job;
+        this.characters = characters.split(",");
+    }
+
+    public String getImbdNameId() {
+        return imbdNameId;
+    }
+
+    public String getImbdTitled() {
+        return imbdTitled;
+    }
+
+    public int getOrdering() {
+        return ordering;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String[] getCharacters() {
+        return characters;
+    }
 }
--- src/Modelo/MovieRating.java
+++ src/Modelo/MovieRating.java
@@ -21,7 +21,7 @@ public class MovieRating implements Comparable<MovieRating> {
     private Rating us;
     private Rating non_us;
 
-/*
+
     private Rating allGenders_0age;
     private Rating allGenders_18age;
     private Rating allGenders_30age;
@@ -39,7 +39,7 @@ public class MovieRating implements Comparable<MovieRating> {
     private Rating top_1000Voters;
     private Rating usVoters;
     private Rating nonUsVoters;
-    */
+
 
     public MovieRating(String imdbTitle, String weighted_average_vote, String total_votes, String mean_votes, String median_votes,
                        String votes_10, String votes_9, String votes_8, String votes_7, String votes_6, String votes_5, String votes_4, String votes_3, String votes_2, String votes_1,
@@ -54,6 +54,54 @@ public class MovieRating implements Comparable<MovieRating> {
     ){
         this.imdbTitle = imdbTitle;
         this.weightedAverage = Float.parseFloat(weighted_average_vote);
+        this.totalVotes = Integer.parseInt(total_votes);
+        this.meanVotes = Float.parseFloat(mean_votes);
+        this.votesRating = new ArrayList<>();
+        votesRating.add(Integer.parseInt(votes_10));
+        votesRating.add(Integer.parseInt(votes_9));
+        votesRating.add(Integer.parseInt(votes_8));
+        votesRating.add(Integer.parseInt(votes_7));
+        votesRating.add(Integer.parseInt(votes_6));
+        votesRating.add(Integer.parseInt(votes_5));
+        votesRating.add(Integer.parseInt(votes_4));
+        votesRating.add(Integer.parseInt(votes_3));
+        votesRating.add(Integer.parseInt(votes_3));
+        votesRating.add(Integer.parseInt(votes_2));
+        votesRating.add(Integer.parseInt(votes_1));
+        this.allGenders_0age = new Rating(Integer.parseInt(allgenders_0age_votes),
+                Float.parseFloat(allgenders_0age_avg_vote));
+        this.allGenders_18age = new Rating(Integer.parseInt(allgenders_18age_votes),
+                Float.parseFloat(allgenders_18age_avg_vote));
+        this.allGenders_30age = new Rating(Integer.parseInt(allgenders_30age_votes),
+                Float.parseFloat(allgenders_30age_avg_vote));
+        this.allGenders_45age = new Rating(Integer.parseInt(allgenders_45age_votes),
+                Float.parseFloat(allgenders_18age_avg_vote));
+        this.malesAllAge = new Rating(Integer.parseInt(males_allages_votes),
+                Float.parseFloat(males_allages_avg_vote));
+        this.males_0age = new Rating(Integer.parseInt(males_0age_votes),
+                Float.parseFloat(males_0age_avg_vote));
+        this.males_18age = new Rating(Integer.parseInt(males_18age_votes),
+                Float.parseFloat(males_18age_avg_vote));
+        this.males_30age = new Rating(Integer.parseInt(males_30age_votes),
+                Float.parseFloat(males_30age_avg_vote));
+        this.males_45age = new Rating(Integer.parseInt(males_45age_votes),
+                Float.parseFloat(males_45age_avg_vote));
+        this.femalesAllAge = new Rating(Integer.parseInt(females_allages_votes),
+                Float.parseFloat(females_allages_avg_vote));
+        this.females_0age = new Rating(Integer.parseInt(females_0age_votes),
+                Float.parseFloat(females_0age_avg_vote));
+        this.females_18age = new Rating(Integer.parseInt(females_18age_votes),
+                Float.parseFloat(females_18age_avg_vote));
+        this.females_30age = new Rating(Integer.parseInt(females_30age_votes),
+                Float.parseFloat(females_30age_avg_vote));
+        this.females_45age = new Rating(Integer.parseInt(females_45age_votes),
+                Float.parseFloat(females_45age_avg_vote));
+        this.top_1000Voters = new Rating(Integer.parseInt(top1000_voters_rating),
+                Float.parseFloat(top1000_voters_vote));
+        this.usVoters = new Rating(Integer.parseInt(us_voters_ratings),
+                Float.parseFloat(us_voters_votes));
+        this.nonUsVoters = new Rating(Integer.parseInt(non_us_voters_ratings),
+                Float.parseFloat(non_us_voters_votes));
 
     }
 
