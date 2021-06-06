26d1f82014ff4581c3bb0ad5c81429fe04c1c36f
Federico Fuidio <ffuidio@correo.um.edu.uy>
2021-06-06 13:40:31 -0300

    MovieRating y Rating constructores

--- src/Modelo/Rating.java
+++ src/Modelo/Rating.java
@@ -5,4 +5,9 @@ public class Rating {
     private Integer NumberVotes;
     private Float AverageRating;
 
+    public Rating(Integer NumberVotes, Float AverageRating){
+        this.NumberVotes = NumberVotes;
+        this.AverageRating = AverageRating;
+    }
+
 }
